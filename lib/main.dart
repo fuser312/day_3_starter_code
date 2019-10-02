f14a5182ee23c83aa46a8f71231345e11f326f30
fuser <sahil.dogstar@gmail.com>
2019-10-02 12:59:08 +0530

    Dart1 Morning Challenge completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  Row row = Row(
+    children: <Widget>[
+      Icon(Icons.call, color: Colors.teal),
+      Text("+91 98012 15835")
+    ],
+  );
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.indigoAccent,
         appBar: AppBar(
           centerTitle: true,
-          backgroundColor: Colors.indigo,
+          backgroundColor: Colors.green,
           title: Text('Business Card'),
         ),
-        body: Center(child: Text('Have a productive day!')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.indigoAccent,
+                radius: 100,
+                backgroundImage: AssetImage("assets/profile.jpg"),
+              ),
+              Text(
+                "Sahil Singh", style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: "Lobster"
+              ),
+              ),
+
+              Text(
+                  "Flutter Developer", style: TextStyle(
+                fontFamily: "Lobster",
+                color: Colors.white,
+                fontSize: 24,
+                letterSpacing: 8,
+              ),
+              ),
+              Card(
+                margin: EdgeInsets.all(20),
+                child : Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: ListTile(
+                    leading: Icon(Icons.call, color: Colors.teal),
+                    title: Text("+919801215835", style: TextStyle(
+                      fontSize: 12
+                    ),),
+                    trailing: IconButton(
+                      icon: Icon(Icons.content_copy),
+                      onPressed: copyContent,
+                    ),
+                ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(20),
+                child : Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: ListTile(
+                    leading: Icon(Icons.mail, color: Colors.teal),
+                    title: Text("sahil.dogstar@gmail.com", style: TextStyle(
+                      fontSize: 12
+                    ),),
+                    trailing: IconButton(
+                      icon: Icon(Icons.content_copy),
+                      onPressed: copyContent,
+                    ),
+                ),
+                ),
+              )
+            ],
       ),
+        ),
     ),
+  ),
   );
+}
+
+void copyContent(){
+  print("Wow, you pressed this Button!");
 }
